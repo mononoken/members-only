c3e7d1373b888385edff68e8344414d251716a45
mononoken <ken.maeshima@pm.me>
2023-05-22 15:31:18 -0700

    Change root to post index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'home#index'
+  # root 'home#index'
+  root 'posts#index'
 
   resources :posts, only: %i[new create index]
 end
